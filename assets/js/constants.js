24fe59efa9698d47869b2ef945c629eae605b109
gamebini <107747775+gamebini@users.noreply.github.com>
2024-11-23 08:15:36 +0900

    Update constants.js

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,1 +1,24 @@
-
+export const CONSTANTS = {
+    SELECTORS: {
+        INITIAL_VIEW: '#initialView',
+        HEADER: '#header',
+        MAIN_CONTENT: '#mainContent',
+        DICTIONARY_CONTENT: '#dictionaryContent',
+        SEARCH_INPUT: '#searchInput',
+        MAIN_SEARCH_INPUT: '#mainSearchInput',
+        MODALS: {
+            INTRO: '#introModal',
+            BETA: '#betaNoticeModal',
+            REQUEST: '#requestModal'
+        }
+    },
+    CLASSES: {
+        VISIBLE: 'visible',
+        SHOW: 'show',
+        MOVE_UP: 'move-up'
+    },
+    STORAGE_KEYS: {
+        VISITED: 'visited',
+        RETURNING_USER: 'returning_user'
+    }
+};
